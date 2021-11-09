7eed3ba232816ddb7af4a198aac41d2b60c34abe
Joao Bernardo Luis <joao.ber.luis@gmail.com>
2021-11-09 14:16:44 +0000

    chore: Updated Git Ignore, Demo App and Extensions as a library
    Added Demo App;
    Added Android Extensions Library;

---
+++ androidextensions/src/main/java/eu/jobernas/extensions/DimensExtensions.kt
@@ -0,0 +1,15 @@
+package eu.jobernas.extensions
+
+import kotlin.math.floor
+
+val Int.dp: Int get() = if (this == 0) 0 else floor(ExtensionsWrapper.density * this.toDouble()).toInt()
+
+val Float.dp: Float get() = if (this == 0f) 0f else floor(ExtensionsWrapper.density * this.toDouble()).toFloat()
+
+val Float.dpF: Float get() = if (this == 0f) 0f else floor(ExtensionsWrapper.density * this.toDouble()).toFloat()
+
+val Int.sp: Int get() = if (this == 0) 0 else floor(ExtensionsWrapper.fontDensity * this.toDouble()).toInt()
+
+val Float.sp: Float get() = if (this == 0f) 0f else floor(ExtensionsWrapper.fontDensity * this.toDouble()).toFloat()
+
+val Float.spInt: Int get() = this.sp.toInt()
